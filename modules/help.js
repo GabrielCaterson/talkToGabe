35abd27b99ec53daf569f958cc8400ea05f56fa9
Gabriel <4404880+GabrielCaterson@users.noreply.github.com>
2021-02-17 20:06:38 -0500

    UI and code readability improvements.

--- modules/help.js
+++ modules/help.js
@@ -3,22 +3,22 @@ const { prefix, token } = require('../config.json');
 
 const content = function(message, color1) {
 	if (message.content === `${prefix}help`) {
-	const helpEmbed = new Discord.MessageEmbed()
-		.setColor(color1)
-		.setTitle('Commands')
-		.setAuthor('by whyZaya')
-		.setDescription("Here\'s what I can do. \n \u200B")
-		.addFields(
+		const helpEmbed = new Discord.MessageEmbed()
+			.setColor(color1)
+			.setTitle('Commands')
+			.setAuthor('by whyZaya')
+			.setDescription("Here\'s what I can do. \n \u200B")
+			.addFields(
 
-			{ name: '**!size**', value: 'Shows the number of people in the server. \n \u200B', inline: true },
-			{ name: '**!dice**', value: 'Rolls dice. You can include a number (like !dice 100) to get a number between 1 and that number. \n \u200B', inline: true },
-			{ name: '**!contradiction**', value: 'Gives an example of one of the problems with contradictions. You can include a proposition to be used in the example. \n \u200B', inline: true },
-			{ name: '**!user-info**', value: 'Shows your user ID. \n \u200B', inline: true },
-			{ name: '**!videos**', value: "Quick access to links to James\' videos. \n \u200B", inline: true },
+				{ name: '**!size**', value: 'Shows the number of people in the server. \n \u200B', inline: true },
+				{ name: '**!dice**', value: 'Rolls dice. You can include a number (like !dice 100) to get a number between 1 and that number. \n \u200B', inline: true },
+				{ name: '**!contradiction**', value: 'Gives an example of one of the problems with contradictions. You can include a proposition to be used in the example. \n \u200B', inline: true },
+				{ name: '**!user-info**', value: 'Shows your user ID. \n \u200B', inline: true },
+				{ name: '**!videos**', value: "Quick access to links to James\' videos. \n \u200B", inline: true },
 
-		)
-		.setTimestamp()
-		.setFooter('I also nag people sometimes. :)');
+			)
+			.setTimestamp()
+			.setFooter('I also nag people sometimes. :)');
 
 		message.channel.send(helpEmbed);
 		return true;
--- modules/monitor.js
+++ modules/monitor.js
@@ -2,22 +2,30 @@ const Discord = require('discord.js');
 const { prefix } = require('../config.json');
 
 const monitorChannel = "797494643431702549";
+const myID = "224703409515790336";
 
 const string = function(client, message, color, client) {
 	function watchFor(user, stringToWatch) {
 		if (message.content.toLowerCase().includes(stringToWatch.toLowerCase()) && !message.author.bot) {
 			
-			client.channels.cache.get(monitorChannel).send(`Message from ${message.author.tag}: ${message.content} \n-Posted in: ${message.channel.name} \n-<@${user}>\n`);
+			client.channels.cache.get(monitorChannel).send(
+				`Message from <@${message.author.id}>: ${message.content}\n\n` +
+				
+				`Posted in: <#${message.channel.id}>\n` +
+				`${message.url}\n` +
+				`<@${user}>\n` +
+				"``` ```"
+			);
 
 		}
 	}
 
 
 	//watchFor("whyZaya");
-	watchFor("224703409515790336", "zaya");
-	watchFor("224703409515790336", "Z*ya");
-	watchFor("224703409515790336", "Za*a");
-	watchFor("224703409515790336", "Z**a");
+	watchFor(myID, "zaya");
+	watchFor(myID, "Z*ya");
+	watchFor(myID, "Za*a");
+	watchFor(myID, "Z**a");
 
 }
 
